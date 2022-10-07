281b229e8e9f4ea5ff2936f580098e9f8f298b0d
emoore8902 <e.moore8902@gmail.com>
2022-10-06 23:40:32 -0700

    Setting up Barebones for project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src={require('./logo192.png')} width = "28.93px"/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
